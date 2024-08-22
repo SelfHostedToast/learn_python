ca1baf0f1b7b881b793e1e69d8bd891bc8b838b6
Dominick Smith <dominick.smith938@gmail.com>
2024-08-22 06:58:38 -0500

    completed task 6-4

--- Chapter 6/glossary.py
+++ Chapter 6/glossary.py
@@ -1,9 +1,6 @@
-glossary = {'variable': 'a placeholder for data in programming', 'conditional statement': 'a statement that evaluates either true or false', 'integer': 'a number in programming', 'for loop': 'a kind of programming loop that runs based on a provided range', 'dictionary': 'an abstraction used in programming to group like items in a "key-value" pair relationship'}
+glossary = {'variable': 'a placeholder for data in programming', 'conditional statement': 'a statement that evaluates either true or false', 'integer': 'a number in programming', 'for loop': 'a kind of programming loop that runs based on a provided range', 'dictionary': 'an abstraction used in programming to group like items in a "key-value" pair relationship', 'method': 'a built-im way of modifying a statement in programming', 'interpreter': 'the "middle-man" of sorts that converts code into machine code for computers to understand', 'list': 'a list is a programming data structure that allows similar data to be stored', 'program': 'a set of instructions for a computer or machine to follow explicitly, developed by a programmer', 'web application': 'a program written in a way that allows it to be interfaced via a web browser'}
 
 print("Glossary of Programming Terminology:\n")
 
-print(f"A Variable is defined as:\n{glossary['variable']}.\n")
-print(f"A Conditional Statement is defined as:\n{glossary['conditional statement']}.\n")
-print(f"A Integer is defined as:\n{glossary['integer']}.\n")
-print(f"A For Loop is defined as:\n{glossary['for loop']}.\n")
-print(f"A Dictionary is defined as:\n{glossary['dictionary']}.\n")
+for word, definition in glossary.items():
+    print(f"A(n) {word} is defined as: {definition}.\n")
