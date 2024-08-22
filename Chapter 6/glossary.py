6892e8c51e986c4e1742bae90b5048fe0a06ac8d
Dominick Smith <dominick.smith938@gmail.com>
2024-08-21 19:59:36 -0500

    completed task 6-3

---
+++ Chapter 6/glossary.py
@@ -0,0 +1,9 @@
+glossary = {'variable': 'a placeholder for data in programming', 'conditional statement': 'a statement that evaluates either true or false', 'integer': 'a number in programming', 'for loop': 'a kind of programming loop that runs based on a provided range', 'dictionary': 'an abstraction used in programming to group like items in a "key-value" pair relationship'}
+
+print("Glossary of Programming Terminology:\n")
+
+print(f"A Variable is defined as:\n{glossary['variable']}.\n")
+print(f"A Conditional Statement is defined as:\n{glossary['conditional statement']}.\n")
+print(f"A Integer is defined as:\n{glossary['integer']}.\n")
+print(f"A For Loop is defined as:\n{glossary['for loop']}.\n")
+print(f"A Dictionary is defined as:\n{glossary['dictionary']}.\n")
