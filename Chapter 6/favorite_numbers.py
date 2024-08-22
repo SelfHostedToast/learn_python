13f90651ea4487516f9edfd6c87907296f3023fb
Dominick Smith <dominick.smith938@gmail.com>
2024-08-21 19:49:31 -0500

    completed task 6-2

---
+++ Chapter 6/favorite_numbers.py
@@ -0,0 +1,7 @@
+favorite_numbers = {'dominick': 21, 'lela': 7, 'destinie': 2, 'johnny': 21, 'jacob': 21}
+
+print(f"Dominick's favorite number is {favorite_numbers['dominick']}!\n")
+print(f"Johnny's favorite number is {favorite_numbers['johnny']}!\n")
+print(f"Lela's favorite number is {favorite_numbers['lela']}!\n")
+print(f"Jacob's favorite number is {favorite_numbers['jacob']}!\n")
+print(f"Destinie's favorite number is {favorite_numbers['destinie']}!\n")
