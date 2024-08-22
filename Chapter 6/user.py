5e4ee4f61097d8ae4fcd5b9fb765f095c8f3b724
Dominick Smith <dominick.smith938@gmail.com>
2024-08-21 20:08:40 -0500

    added dict loop using items method

---
+++ Chapter 6/user.py
@@ -0,0 +1,11 @@
+user_0 = {
+    'username': 'SelfHostedToast',
+    'first': 'Dominick',
+    'last': 'Smith',
+}
+
+# Using the items() method (returns the key, value pair of a dictionary) we loop through the dictionary, assigning each pair to its respective key or value and printing them as such.
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
