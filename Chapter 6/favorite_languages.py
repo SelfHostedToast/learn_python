e65c6d564f9e9f38038fa216c7f26e8cccffaa5c
Dominick Smith <dominick.smith938@gmail.com>
2024-08-22 06:17:06 -0500

    loop update

--- Chapter 6/favorite_languages.py
+++ Chapter 6/favorite_languages.py
@@ -8,3 +8,8 @@ favorite_languages = {
 
 language = favorite_languages['dominick'].title()
 print(f"Dominick's favorite language is {language}.")
+
+# Looping through the dictionary, because the keys and values are the same type, we can generalize them into unique variable names for key and value respectively.
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
