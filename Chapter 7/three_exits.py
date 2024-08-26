a50f7bf020f8a5697548f935744056e201ac929c
Dominick Smith <dominick.smith938@gmail.com>
2024-08-26 13:32:42 -0500

    completed task 7-6

--- Chapter 7/three_exits.py
+++ Chapter 7/three_exits.py
@@ -1,2 +1,16 @@
 # 7-6 Three Exits
 
+prompt = "Enter a topping to add to your custom pizza!"
+prompt += "\nEnter 'quit' to stop editing your pizza: "
+
+editing_pizza = True
+
+while editing_pizza:
+    pizza_toppings = input(prompt)
+    if pizza_toppings != 'quit':
+        print(f"Adding {pizza_toppings} to your pizza!\n")
+    elif pizza_toppings == 'quit':
+        print("Exiting...")
+        break
+    else:
+        editing_pizza = False
