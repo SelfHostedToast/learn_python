3e0e909fea3af029d0df7eda47aae180af5ded17
Dominick Smith <dominick.smith938@gmail.com>
2024-08-27 13:01:53 -0500

    completed task 8-3

---
+++ Chapter 8/t_shirt.py
@@ -0,0 +1,8 @@
+# 8-3 T-shirt
+
+def make_shirt(size, message):
+    print(f"Starting the process of making a {size} t-shirt with the message {message} on the front.")
+
+make_shirt('large', 'I love Python')
+
+make_shirt(size='large', message='I love Python')
