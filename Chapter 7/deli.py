b827fe0f18181908d5109f19fd9e72bbf394ce70
Dominick Smith <dominick.smith938@gmail.com>
2024-08-27 07:27:57 -0500

    completed task 7-8

--- Chapter 7/deli.py
+++ Chapter 7/deli.py
@@ -1,1 +1,13 @@
 # 7-8 Deli
+
+sandwich_orders = ['blt', 'ham and cheddar', 'turkey bacon ranch', 'ham and provolone']
+
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I am preparing the {current_sandwich}")
+    finished_sandwiches.append(current_sandwich)
+
+for sandwich in finished_sandwiches:
+    print(sandwich)
