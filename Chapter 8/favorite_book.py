29723cef7710bc0e40225993f87f12aeb8156c12
Dominick Smith <dominick.smith938@gmail.com>
2024-08-27 11:59:26 -0500

    completed task 8-2

---
+++ Chapter 8/favorite_book.py
@@ -0,0 +1,8 @@
+# 8-2 Favorite Book
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+fav_book_prompt = input("What is the title of your favorite book? ")
+
+favorite_book(fav_book_prompt)
