6ffd9d79fce95ef958d5e4b4d271b35e080260e6
Dominick Smith <dominick.smith938@gmail.com>
2024-08-28 06:30:17 -0500

    optional function arguments

--- Chapter 8/formatted_name.py
+++ Chapter 8/formatted_name.py
@@ -1,7 +1,12 @@
-def get_formatted_name(first_name, last_name):
+def get_formatted_name(first_name, last_name, middle_name=''):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
 programmer = get_formatted_name('dominick', 'smith')
+print(programmer)
+programmer = get_formatted_name('dominick', 'smith', 'cristian')
 print(programmer)
