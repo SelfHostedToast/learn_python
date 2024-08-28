53f2799d4b71d0168786ad6b395f461348080e20
Dominick Smith <dominick.smith938@gmail.com>
2024-08-28 06:17:30 -0500

    completed task 8-5

---
+++ Chapter 8/cities.py
@@ -0,0 +1,8 @@
+# 8-5 Cities
+
+def describe_city(city_name, country='United States'):
+    print(f"The city of {city_name.title()} is in the country of {country.title()}!")
+
+describe_city('dallas')
+describe_city('new york')
+describe_city('tokyo', 'japan')
