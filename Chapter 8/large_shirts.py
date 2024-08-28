8d38e4afa8cf8c8d42a96cd67a3dd380ef62bdd7
Dominick Smith <dominick.smith938@gmail.com>
2024-08-28 06:13:01 -0500

    completed task 8-4

---
+++ Chapter 8/large_shirts.py
@@ -0,0 +1,8 @@
+# 8-4 Large Shirts
+
+def make_shirt(size='large', message='I love Python'):
+    print(f"Making your {size} shirt with the message '{message}' on the front.")
+
+make_shirt()
+make_shirt('medium')
+make_shirt('small', 'I love Python so much!')
