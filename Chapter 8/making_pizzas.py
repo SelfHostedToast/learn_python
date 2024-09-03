19ff60b46862939ab06e9d8c6430501fdd54e496
Dominick Smith <dominick.smith938@gmail.com>
2024-09-03 06:52:12 -0500

    importing modules

---
+++ Chapter 8/making_pizzas.py
@@ -0,0 +1,10 @@
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'philly steak', 'alfredo sauce', 'bacon', 'three cheese blend')
+
+# You can import specific functions from a module
+# from module_name import function_name
+# This can be done for as many functions as you want
+# from module_name import function_0, function_1, function_2
+
