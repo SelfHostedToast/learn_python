d0d14eb2c6aaa4015bf5a39b7893539d7fa9b328
Dominick Smith <dominick.smith938@gmail.com>
2024-09-03 07:16:08 -0500

    module aliases

---
+++ Chapter 8/making_pizzas2.py
@@ -0,0 +1,14 @@
+from pizza import make_pizza
+
+make_pizza(16, 'pepperoni', 'bacon', 'sausage')
+
+# You can give imported functions an alias
+# from module_name import function as alias
+
+# The same can be done for module aliases
+# import module_name as alias
+
+# Lastly, you can import all of a modules functions by using an *
+
+# from module_name import *
+
