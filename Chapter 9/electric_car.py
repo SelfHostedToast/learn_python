bd97ca2abc31a23218d13d0cf55750309c4cb66a
Dominick Smith <dominick.smith938@gmail.com>
2024-09-08 08:43:53 -0500

    started task 9-6

--- Chapter 9/electric_car.py
+++ Chapter 9/electric_car.py
@@ -28,6 +28,25 @@ class Car:
         """Add the given amount tothe odometer reading."""
         self.odometer_reading += miles
 
+class Battery:
+    """A simple attempt to model a bettery for an electric car."""
+
+    def __init__(self, battery_size=40):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size <= 40:
+            range = 150
+        elif self.battery_size > 40:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
@@ -36,12 +55,19 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 43
+        self.battery = Battery()
 
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+    def fill_gas_tank(self):
+        """Electric cars don't have gas tanks."""
+        print("This car doesn't have a gas tank!")
 
+print('Cars in my future garage:\n')
 my_electric_car = ElectricCar('tesla', 'cyber truck', '2024')
+my_electric_car.battery.battery_size=43
 print(my_electric_car.get_descriptive_name())
-my_electric_car.describe_battery()
+my_electric_car.battery.describe_battery()
+my_electric_car.fill_gas_tank()
+my_electric_car.battery.get_range()
+
+my_gas_car = Car('kia', 'soul', '2016')
+print(my_gas_car.get_descriptive_name())
