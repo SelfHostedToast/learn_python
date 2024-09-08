a8d90dbde989633d981985ee5ee83e97b2544f03
Dominick Smith <dominick.smith938@gmail.com>
2024-09-08 11:43:09 -0500

    completed task 9-8

--- Chapter 9/admin.py
+++ Chapter 9/admin.py
@@ -5,6 +5,7 @@ class User:
         self.last_name = last_name
         self.city = city
         self.age = age
+        self.privilege = Privileges()
         
     def describe_user(self):
         print(f"----User information for {self.first_name} {self.last_name}----")
@@ -19,14 +20,28 @@ class User:
 class Admin(User):
     def __init__(self, first_name, last_name, city, age):
         super().__init__(first_name, last_name, city, age)
-        self.privileges = ['can add posts', 'can delete posts', 'can modify site', 'can moderate users']
+        self.privilege = Privileges(['can add posts', 'can delete posts', 'can modify site', 'can moderate users'])
+
+class Privileges():
+
+    def __init__(self, privileges=['can add posts', 'can delete own posts']):
+        self.privileges = privileges
 
     def show_privileges(self):
-        print(f'\n{self.first_name} can perform the following actions:')
+
+        
         for privilege in self.privileges:
                 print(f"\t-{privilege}")
 
-dom = Admin('Dominick', 'Smith', 'Carrollton', 23)
-dom.show_privileges()
-johnny = User('Johnny', 'Ortiz', 'Oak Point', 24)
-jacob = User('Jacob', 'Castillo', 'Dallas', 25)
+user_1 = Admin('Dominick', 'Smith', 'Carrollton', 23)
+print(f'\n{user_1.first_name} can perform the following actions:')
+user_1.privilege.show_privileges()
+
+user_2 = User('Johnny', 'Ortiz', 'Oak Point', 24)
+print(f'\n{user_2.first_name} can perform the following actions:')
+user_2.privilege.show_privileges()
+
+user_3 = User('Jacob', 'Castillo', 'Dallas', 25)
+print(f'\n{user_3.first_name} can perform the following actions:')
+user_3.privilege.show_privileges()
+
